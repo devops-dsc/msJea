4855ad523365400273242e0850c1582df203de06
Jon Madison <Jonathan.madison@rackspace.com>
2015-01-09 15:18:58 -0600

    refactor

--- Examples/Demo2.ps1
+++ Examples/Demo2.ps1
@@ -1,9 +1,9 @@
-﻿cls
+cls
 configuration Demo2
 {
     Import-DscResource -module xjea
 
-    xJeaToolKit SMBGet
+    msJeaToolKit SMBGet
     {
         Name         = 'SMBGet'
         CommandSpecs = @"
@@ -11,12 +11,12 @@ Module,Name,Parameter,ValidateSet,ValidatePattern
 SMBShare,get-*
 "@
     }
-    xJeaEndPoint Demo2EP
+    msJeaEndPoint Demo2EP
     {
         Name                   = 'Demo2EP'
         Toolkit                = 'SMBGet'
         SecurityDescriptorSddl = 'O:NSG:BAD:P(A;;GX;;;WD)S:P(AU;FA;GA;;;WD)(AU;SA;GXGW;;;WD)'                                  
-        DependsOn              = '[xJeaToolKit]SMBGet'
+        DependsOn              = '[msJeaToolKit]SMBGet'
     }
 
 
